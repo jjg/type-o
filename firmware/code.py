67dca197b3c439558bda415b7f1664ab80b81951
Jason J. Gullickson <mr@jasongullickson.com>
2023-11-12 15:08:21 -0600

    Refactor scanning.

--- firmware/code.py
+++ firmware/code.py
@@ -7,27 +7,51 @@ import microcontroller
 from digitalio import DigitalInOut, Direction, DriveMode, Pull
 
 # init keyboard
-row_1 = DigitalInOut(microcontroller.pin.GPIO18)
-row_1.direction = Direction.OUTPUT
-row_1.drive_mode = DriveMode.OPEN_DRAIN
-row_1.value = False
 
-row_2 = DigitalInOut(microcontroller.pin.GPIO25)
-row_2.direction = Direction.OUTPUT
-row_2.drive_mode = DriveMode.OPEN_DRAIN
-row_2.value = False
+# map keys to characters ([row][column])
+r = 4 
+c = 12
+keymap = [[0 for x in range(r)] for y in range(c)]
+keymap[0][0] = "c"
+keymap[1][0] = "?"
+keymap[0][1] = "q"
+keymap[1][1] = "a"
 
-#row_2 = DigitalInOut(microcontroller.pin.GPIO25)
+# row GPIOs
+row_gpios = [
+        microcontroller.pin.GPIO18,
+        microcontroller.pin.GPIO25
+        ]
+
+# init rows
+kbd_rows = []
+for gpio in row_gpios:
+    row = DigitalInOut(gpio)
+    row.direction = Direction.OUTPUT
+    row.drive_mode = DriveMode.OPEN_DRAIN
+    row.value = False
+    kbd_rows.append(row)
+
+# TODO: Add remaining rows
 #row_3 = DigitalInOut(microcontroller.pin.GPIO24)
 #row_4 = DigitalInOut(microcontroller.pin.GPIO29)
 
-col_1 = DigitalInOut(microcontroller.pin.GPIO12)
-col_1.direction = Direction.INPUT
-col_1.pull = Pull.UP
+# col GPIOs
+col_gpios = [
+        microcontroller.pin.GPIO12,
+        microcontroller.pin.GPIO13
+        ]
+# init cols
+kbd_cols = []
+for gpio in col_gpios:
+    col = DigitalInOut(gpio)
+    col.direction = Direction.INPUT
+    col.pull = Pull.UP
+    kbd_cols.append(col)
 
-col_2 = DigitalInOut(microcontroller.pin.GPIO13)
-col_2.direction = Direction.INPUT
-col_2.pull = Pull.UP
+# keyboard input buffers
+kbd_char = ""
+kbd_last_char = ""
 
 # main event loop
 while True: 
@@ -35,21 +59,39 @@ while True:
     # TODO: Read keypress from keyboard
     #print("Entering keyboard scan")
 
-    row_1.value = False # Connect drain to ground to scan the row
-    #print(col_1.value)  # False if pressed (pulled to ground)
-    if col_1.value == False:
-        print("crop key pressed")
-    if col_2.value == False:
-        print("q key pressed")
-    row_1.value = True  # Disconnect drain from ground to stop scanning row
+    keymap_x = 0
+    keymap_y = 0
+    for kbd_row in kbd_rows:
+        kbd_row.value = False   # connect the row
+        for kbd_col in kbd_cols:
+            if kbd_col.value == False:
+                kbd_char = keymap[keymap_x][keymap_y]
+            keymap_y = keymap_y + 1
+        kbd_row.value = True    # disconnect the row
+        keymap_y = 0
+        keymap_x = keymap_x + 1
 
-    row_2.value = False # Connect drain to ground to scan the row
+    print(kbd_char)
+
+    #if kbd_char != kbd_last_char:
+    #    print(kbd_char)
+    #    kbd_last_char = kbd_char
+
+    #row_1.value = False # Connect drain to ground to scan the row
     #print(col_1.value)  # False if pressed (pulled to ground)
-    if col_1.value == False:
-        print("weird key pressed")
-    if col_2.value == False:
-        print("a key pressed")
-    row_2.value = True  # Disconnect drain from ground to stop scanning row
+    #if col_1.value == False:
+    #    print("crop key pressed")
+    #if col_2.value == False:
+    #    print("q key pressed")
+    #row_1.value = True  # Disconnect drain from ground to stop scanning row
+
+    #row_2.value = False # Connect drain to ground to scan the row
+    #print(col_1.value)  # False if pressed (pulled to ground)
+    #if col_1.value == False:
+    #    print("weird key pressed")
+    #if col_2.value == False:
+    #    print("a key pressed")
+    #row_2.value = True  # Disconnect drain from ground to stop scanning row
 
     # TODO: Initialize display
     #import busio
